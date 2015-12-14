9b8405c699f44ef8b8ffd2d8c7b15c8e8314e4a7
Orlando L Otero <ootero@3velopers.net>
2015-12-13 21:35:19 -0500

    Changed - Removing asimio parent pom - Uplifted spring boot, cloud versions [No ticket]

--- src/main/java/com/asimio/cloud/eureka/EurekaServerApplication.java
+++ src/main/java/com/asimio/cloud/eureka/EurekaServerApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableEurekaServer
-@ImportResource("classpath:Metrics.xml")
 public class EurekaServerApplication extends SpringBootServletInitializer {
 
 	@Override
