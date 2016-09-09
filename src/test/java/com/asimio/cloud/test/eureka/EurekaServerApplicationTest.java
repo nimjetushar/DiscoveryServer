56ed620c5b9c876c31e920fbe6a2b61360bdf435
Orlando L Otero <ootero@asimio.net>
2016-09-09 00:07:41 -0400

    Changed - Upgraded Spring Boot to 1.4.0.RELEASE, Spring Cloud to Brixton.SR5 - Fixed deprecated annotations in integration test

--- src/test/java/com/asimio/cloud/test/eureka/EurekaServerApplicationTest.java
+++ src/test/java/com/asimio/cloud/test/eureka/EurekaServerApplicationTest.java
@@ -4,25 +4,22 @@ import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.asimio.cloud.eureka.EurekaServerApplication;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = EurekaServerApplication.class)
-@WebAppConfiguration
-@IntegrationTest("server.port:0")
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EurekaServerApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("standalone")
 public class EurekaServerApplicationTest {
 
-	@Value("${local.server.port}")
+	@LocalServerPort
 	private int port;
 
 	@Before
